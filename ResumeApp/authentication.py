f748b00244b1e1195994c3bbcaca2f88726d4b50
A V K Shaileshwaran <shaileshwaranavk@gmail.com>
2025-07-27 21:52:23 +0530

    Update authentication.py

--- ResumeApp/authentication.py
+++ ResumeApp/authentication.py
@@ -1,34 +1,36 @@
-# ResumeApp/authentication.py
+# ResumeApp/auth.py
 
-import jwt
 from rest_framework.authentication import BaseAuthentication
 from rest_framework import exceptions
 from django.conf import settings
+import jwt
+
 from .models import User_Details
 
-class JWTAuthentication(BaseAuthentication):
+class CustomJWTAuthentication(BaseAuthentication):
     def authenticate(self, request):
         auth_header = request.headers.get('Authorization')
+
         if not auth_header:
             return None
 
         try:
             prefix, token = auth_header.split(' ')
             if prefix.lower() != 'bearer':
-                return None
+                raise exceptions.AuthenticationFailed('Invalid token prefix')
         except ValueError:
-            return None
+            raise exceptions.AuthenticationFailed('Invalid Authorization header format')
 
         try:
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
         except jwt.ExpiredSignatureError:
-            raise exceptions.AuthenticationFailed('Token expired.')
+            raise exceptions.AuthenticationFailed('Token expired')
         except jwt.InvalidTokenError:
-            raise exceptions.AuthenticationFailed('Invalid token.')
+            raise exceptions.AuthenticationFailed('Invalid token')
 
         try:
             user = User_Details.objects.get(id=payload['user_id'])
         except User_Details.DoesNotExist:
-            raise exceptions.AuthenticationFailed('User not found.')
+            raise exceptions.AuthenticationFailed('User not found')
 
         return (user, None)
